03ca44ba051476e65e162a2d2f5dc3a76fc1cb00
sagor31h2 <sagor31h2@gmail.com>
2024-05-22 16:44:05 +0600

    add controller

--- Modules/WaterSubMeter/WaterSubMeter/Controllers/MeterController.cs
+++ Modules/WaterSubMeter/WaterSubMeter/Controllers/MeterController.cs
@@ -2,7 +2,14 @@
 
 namespace WaterSubMeter.Controllers
 {
+    [ApiController]
+    [Route("/api/catalog/[controller]")]
     internal class MeterController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World!");
+        }
     }
 }
