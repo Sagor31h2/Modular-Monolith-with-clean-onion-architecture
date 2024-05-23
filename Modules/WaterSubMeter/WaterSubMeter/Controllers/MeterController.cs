7e82cef91b993c7798996ffd341b5a5354518448
sagor31h2 <sagor31h2@gmail.com>
2024-05-23 23:32:39 +0600

    add mediatr sevice

--- Modules/WaterSubMeter/WaterSubMeter/Controllers/MeterController.cs
+++ Modules/WaterSubMeter/WaterSubMeter/Controllers/MeterController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using WaterSubMeter.core.Database.Abstract;
 using WaterSubMeter.core.Entity;
+using WaterSubMeter.core.Queries;
 using WaterSubMeter.core.Service;
 
 namespace WaterSubMeter.Controllers
@@ -11,16 +13,21 @@ namespace WaterSubMeter.Controllers
     {
         private readonly TestService testService;
         private readonly IWaterSubmeterContext waterSubmeter;
+        private readonly IMediator mediatR;
 
-        public MeterController(IWaterSubmeterContext waterSubmeter)
+        public MeterController(IWaterSubmeterContext waterSubmeter, IMediator mediatR)
+        
         {
             this.testService = new TestService(waterSubmeter);
             this.waterSubmeter = waterSubmeter;
+            this.mediatR = mediatR;
         }
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(await testService.getall());
+            var query = new waterpumpRequest();
+            return Ok(await mediatR.Send(query));
+            //return Ok(await testService.getall());
         }
 
         [HttpPost]
---
+++ Modules/WaterSubMeter/WaterSubmeter.core/Handlers/WaterSubmeterRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WaterSubMeter.core.Database.Abstract;
+using WaterSubMeter.core.Entity;
+using WaterSubMeter.core.Queries;
+
+namespace WaterSubMeter.core.Handlers
+{
+    internal class WaterSubmeterRequestHandler : IRequestHandler<waterpumpRequest, List<WaterSubmeter>>
+    {
+        private readonly IWaterSubmeterContext context;
+
+        public WaterSubmeterRequestHandler(IWaterSubmeterContext context)
+        {
+            this.context = context;
+        }
+
+        async Task<List<WaterSubmeter>> IRequestHandler<waterpumpRequest, List<WaterSubmeter>>.Handle(waterpumpRequest request, CancellationToken cancellationToken)
+        {
+            var list = await context.Meters.ToListAsync();
+            return list;
+        }
+    }
+}
