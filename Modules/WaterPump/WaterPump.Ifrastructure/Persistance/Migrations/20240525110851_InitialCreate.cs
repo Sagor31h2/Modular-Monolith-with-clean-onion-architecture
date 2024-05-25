6aef3b09e47b6e3167125a2b402e0a52baaddff5
sagor31h2 <sagor31h2@gmail.com>
2024-05-25 17:10:43 +0600

    migration water pump

---
+++ Modules/WaterPump/WaterPump.Ifrastructure/Persistance/Migrations/20240525110851_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaterPump.Ifrastructure.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "waterPump");
+
+            migrationBuilder.CreateTable(
+                name: "WaterPumpInfos",
+                schema: "waterPump",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WaterPumpNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    PumpType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaterPumpInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WaterPumpInfos",
+                schema: "waterPump");
+        }
+    }
+}
